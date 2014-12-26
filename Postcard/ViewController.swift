0f2856dd85d1cc26fb293884550dbf4cbc8f03ab
Eliot Arntz <eliotarntz@Eliots-MBP.home>
2014-12-25 19:17:37 -0500

    repo

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    
+    @IBOutlet weak var mailButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden = false
+        messageLabel.text = enterMessageTextField.text
+        messageLabel.textColor = UIColor.redColor()
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        
+        enterMessageTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+        
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Highlighted)
+        mailButton.setTitleColor(UIColor.greenColor(), forState: UIControlState.Normal)
+        mailButton.backgroundColor = UIColor.redColor()
+    }
+    
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool // called when 'return' key pressed. return NO to ignore.
+    {
+        
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Highlighted)
+        mailButton.setTitleColor(UIColor.greenColor(), forState: UIControlState.Normal)
+        mailButton.backgroundColor = UIColor.redColor()
+
+//        textField.resignFirstResponder()
+        return true;
+    }
 
 }
 
